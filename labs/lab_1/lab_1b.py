7d511ffd8258999899c88d5296e046b2d1ea6b01
Eat-thePizza <ethanliu858@gmail.com>
2026-02-21 11:28:55 -0800

    Update lab_1b.py

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -58,8 +58,10 @@ def main():
     while (not isNum(num2)):
         num2 = input("Enter the first number: ")
     num2 = float(num2)
-    
+
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+    while operation not in ("add","subtract","multiply","divide"):
+        operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
     # Perform the calculation and display the result
     result = simple_calculator(operation, num1, num2)
